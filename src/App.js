7f1d16a568d642a5b7ec33e57432c06c52951c83
Michael Bruce Allen <CodeAmend@Gmail.Com>
2019-07-26 13:59:47 -0400

    Prop drilling nightmare (ready to convert)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import immer from 'immer';
+
 import Flash from "./components/Flash";
 
 
@@ -7,7 +9,7 @@ const makeTodo = todo => ({
   ...todo
 });
 
-const todos = [
+const initialState = [
   makeTodo({
     name: "Milk"
   }),
@@ -24,37 +26,66 @@ const todos = [
 ];
 
 export default function App() {
+  const [todos, setTodos] = React.useState(initialState);
+  const [filterType, setFilterType] = React.useState('pending');
+
   return (
     <div>
-      <FilterBar />
+      <FilterBar filterType={filterType} setFilterType={setFilterType} />
       <br />
-      <Todos />
+      <Todos todos={todos} filterType={filterType} setTodos={setTodos} />
       <br />
-      <AddTodo />
+      <AddTodo setTodos={setTodos} />
     </div>
   );
 }
 
-function FilterBar() {
+function FilterBar({ filterType, setFilterType }) {
 
   return (
     <div>
-      <Filter type="pending" > Pending </Filter>
-      <Filter type="done" > Done </Filter>
-      <Filter type="all" > All </Filter>
+      <Filter
+        onClick={() => setFilterType('pending')}
+        active={filterType === 'pending'}
+      > Pending </Filter>
+      <Filter
+        onClick={() => setFilterType('done')}
+        active={filterType === 'done'}
+      > Done </Filter>
+      <Filter
+        onClick={() => setFilterType('all')}
+        active={filterType === 'all'}
+      > All </Filter>
     </div>
   );
 }
 
 function Filter({ active, children, ...rest }) {
+
   return (
-    <Flash {...rest} style={{ background: active ? "yellow" : "initial" }} type="button" >
+    <Flash
+      type="button"
+      {...rest} style={{ background: active ? "yellow" : "initial" }}
+    >
       {children}
     </Flash>
   );
 }
 
-function Todos() {
+function Todos({ todos, setTodos, filterType }) {
+  let filteredTodos;
+
+  switch (filterType) {
+    case 'pending':
+      filteredTodos = todos.filter(t => !t.done);
+      break;
+    case 'done':
+      filteredTodos = todos.filter(t => t.done);
+      break;
+    case 'all':
+      filteredTodos = todos;
+      break;
+  }
   return (
     <Flash
       type="div"
@@ -62,29 +93,40 @@ function Todos() {
         padding: ".25rem"
       }}
     >
-      {todos.map(todo => (
-        <Todo key={todo.id} {...todo} />
+      {filteredTodos.map(todo => (
+        <Todo key={todo.id} {...todo} setTodos={setTodos} />
       ))}
     </Flash>
   );
 }
 
-function Todo({ id, name, done }) {
+function Todo({ id, name, done, setTodos }) {
+  const handleToggle = () => {
+    setTodos(oldTodos => immer(oldTodos, draft => {
+      const todo = draft.find(d => d.id === id);
+      todo.done = !todo.done;
+    }));
+  }
+
   return (
     <Flash
       type="div"
       style={{ padding: ".25rem", textDecoration: done ? "line-through" : "none" }}
     >
-      <button>✅</button> {name}
+      <button onClick={handleToggle}>✅</button> {name}
     </Flash>
   );
 }
 
-function AddTodo() {
+function AddTodo({ setTodos }) {
 
   const [value, setValue] = React.useState("");
 
   const addTodo = () => {
+    setTodos(old => ([
+      ...old,
+      makeTodo({ name: value }),
+    ]))
     setValue('');
   }
 
